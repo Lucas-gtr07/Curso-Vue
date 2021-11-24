6306efacfe38d0ea6abb6602560fd5ef86c6d252
Lucas Prudencio <lucas.menezes.prudencio@outlook.com>
2021-11-24 17:02:03 -0300

    new file:   2_projeto_vue/.browserslistrc 	new file:   2_projeto_vue/.editorconfig 	new file:   2_projeto_vue/.eslintrc.js 	new file:   2_projeto_vue/.gitignore 	new file:   2_projeto_vue/README.md 	new file:   2_projeto_vue/babel.config.js 	new file:   2_projeto_vue/package-lock.json 	new file:   2_projeto_vue/package.json     new file:   2_projeto_vue/public/favicon.ico 	new file:   2_projeto_vue/public/index.html 	new file:   2_projeto_vue/src/App.vue 	new file:   2_projeto_vue/src/assets/logo.png 	new file:   2_projeto_vue/src/components/LifeCycle.vue 	new file:   2_projeto_vue/src/components/PrimeiroComponente.vue 	new file:   2_projeto_vue/src/main.js

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+    <p>O Dado de app é: {{ testando }}</p>
+    <LifeCycle />
+  </div>
+</template>
+
+<script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
+export default {
+  name: 'App',
+  components: {
+    PrimeiroComponente,
+    LifeCycle
+  },
+  data () {
+    return {
+      testando: 'teste'
+    }
+  }
+}
+</script>
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <h1>LifeCycle</h1>
+  <p>Meu nome é: {{ nome }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LyfeCycle',
+  data () {
+    return {
+      nome: 'Ainda não sei'
+    }
+  }
+}
+</script>
